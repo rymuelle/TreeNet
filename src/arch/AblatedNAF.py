3f93164dde86441bc331bede00d5e5fc3b489a2c
Ryan Mueller <ryan.dalrymple.mueller@gmail.com>
2025-11-16 11:18:52 -0500

    up and down layers

--- src/arch/AblatedNAF.py
+++ src/arch/AblatedNAF.py
@@ -1,4 +1,3 @@
-
 # ------------------------------------------------------------------------
 # Modified from NAFNet
 # ------------------------------------------------------------------------
@@ -13,11 +12,8 @@ Simple Baselines for Image Restoration
   year={2022}
 }
 '''
-
-
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from .utils import *
 
 class SimpleGate(nn.Module):
@@ -45,6 +41,55 @@ class NAFBlock(nn.Module):
 
         ffn_channel = FFN_Expand * c
         self.conv4 = nn.Conv2d(in_channels=c, out_channels=ffn_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.conv5 = nn.Conv2d(in_channels=ffn_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+
+        self.norm1 = LayerNorm2d(c)
+        self.norm2 = LayerNorm2d(c)
+
+        self.dropout1 = nn.Dropout(drop_out_rate) if drop_out_rate > 0. else nn.Identity()
+        self.dropout2 = nn.Dropout(drop_out_rate) if drop_out_rate > 0. else nn.Identity()
+
+        self.beta = nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
+        self.gamma = nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
+
+    def forward(self, inp):
+        x = inp
+
+        x = self.norm1(x)
+
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.sg(x)
+        x = x * self.sca(x)
+
+        y = inp + x * self.beta
+
+        # x = self.conv4(self.norm2(y))
+        # x = self.sg(x)
+        # x = self.conv5(x)
+        return y + x * self.gamma
+
+class NAFDown(nn.Module):
+    def __init__(self, c, DW_Expand=2, FFN_Expand=2, drop_out_rate=0.):
+        super().__init__()
+        dw_channel = c * DW_Expand
+        self.conv1 = nn.Conv2d(in_channels=c, out_channels=dw_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.conv2 = nn.Conv2d(in_channels=dw_channel, out_channels=dw_channel, kernel_size=3, padding=1, stride=1, groups=dw_channel,
+                               bias=True)
+
+        # Simplified Channel Attention
+        self.sca = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(in_channels=dw_channel // 2, out_channels=dw_channel // 2, kernel_size=1, padding=0, stride=1,
+                      groups=1, bias=True),
+        )
+
+        # SimpleGate
+        self.sg = SimpleGate()
+
+        ffn_channel = FFN_Expand * c
+        self.conv4 = nn.Conv2d(in_channels=c, out_channels=ffn_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.conv5 = nn.Conv2d(in_channels=ffn_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
 
         self.norm1 = LayerNorm2d(c)
         self.norm2 = LayerNorm2d(c)
@@ -69,6 +114,7 @@ class NAFBlock(nn.Module):
 
         x = self.conv4(self.norm2(y))
         x = self.sg(x)
+        x = self.conv5(x)
         return y + x * self.gamma
 
 
@@ -115,7 +161,7 @@ class AblatedNAF(nn.Module):
             chan = chan // 2
             self.decoders.append(
                 nn.Sequential(
-                    *[NAFBlock(chan) for _ in range(num)]
+                    *[NAFDown(chan) for _ in range(num)]
                 )
             )
 
@@ -152,4 +198,5 @@ class AblatedNAF(nn.Module):
         mod_pad_w = (self.padder_size - w % self.padder_size) % self.padder_size
         x = F.pad(x, (0, mod_pad_w, 0, mod_pad_h))
         return x
+
 
