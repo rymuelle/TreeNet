88ea69487df5e892dde05ee94f4625e7b61822ab
Ryan Mueller <ryan.dalrymple.mueller@gmail.com>
2025-11-18 14:58:20 -0500

    Added local entropy loss

--- src/utils/losses.py
+++ src/utils/losses.py
@@ -8,7 +8,7 @@ def psnr(mse_loss):
     return -10.0 * math.log10(mse_loss) if mse_loss > 0 else 100.0
 
 class PSNRLoss(nn.Module):
-    '''Taken from NAFNet'''
+    '''From NAFNet'''
     def __init__(self, loss_weight=1.0, reduction='mean', toY=False):
         super(PSNRLoss, self).__init__()
         assert reduction == 'mean'
@@ -35,3 +35,51 @@ class PSNRLoss(nn.Module):
         return self.loss_weight * self.scale * torch.log(((pred - target) ** 2).mean(dim=(1, 2, 3)) + 1e-8).mean()
 
 loss_fn = PSNRLoss()
+
+
+class LocalEntropy(nn.Module):
+    def __init__(self, kernel_size=5, stablization = 1e-6):
+        super().__init__()
+        padding = kernel_size // 2
+        self.constant = 2 * math.pi * math.e
+        self.kernel_size = kernel_size
+        self.unfold = nn.Unfold((kernel_size, kernel_size), dilation=1, padding=padding, stride=1)
+        self.stablization = stablization
+        
+    def forward(self, x):
+        B, C, H, W = x.shape
+        K = self.kernel_size
+        patches_flat = self.unfold(x)
+        patches_reshaped = patches_flat.view(B, C, K, K, H, W)
+        var = patches_reshaped.var(dim=(2, 3), keepdim=False) + self.stablization
+        entropy = 1/2 * torch.log(self.constant * var)
+        return entropy
+
+
+class LocalEntropyLoss(nn.Module):
+    def __init__(self, kernel_size=5, criterion=nn.MSELoss):
+        super().__init__()
+        self.le = LocalEntropy(kernel_size=kernel_size)
+        self.criterion = criterion()
+
+    def forward(self, pred, target):
+        ep = self.le(pred)
+        et = self.le(target)
+        loss = self.criterion(ep, et)
+        return loss
+    
+
+class PSNR_LE_loss(nn.Module):
+    def __init__(self, kernel_size=5, criterion=nn.MSELoss, weights=[1, 1]):
+        super().__init__()
+        self.psnr_loss = PSNRLoss()
+        self.le_loss = LocalEntropyLoss(kernel_size=kernel_size, criterion=criterion)
+        self.weights = weights
+
+    def forward(self, pred, target):
+        loss = 0
+        if self.weights[0] > 0: 
+            loss += self.psnr_loss(pred, target)
+        if self.weights[1] > 0: 
+            loss += self.le_loss(pred, target) + self.weights[1]
+        return loss
