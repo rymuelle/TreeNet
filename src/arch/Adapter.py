766c92a7919d778426f8c926d93bef31bbb279f9
Ryan Mueller <ryan.dalrymple.mueller@gmail.com>
2026-04-21 18:38:26 -0400

    Adapter model for testing

---
+++ src/arch/Adapter.py
@@ -0,0 +1,37 @@
+import torch.nn as nn
+import torch
+from src.arch.NAFNet import NAFBlock
+
+class Adapter(nn.Module):
+    def __init__(self, rgb_multiplier=[2., 1. ,2.],
+                 gamma=1./2, intro_blocks=0, outro_blocks=0, width=32):
+        super().__init__()
+
+        self.intro = nn.Sequential(nn.Conv2d(3, width, kernel_size=1),
+            nn.GELU(),
+            *[NAFBlock(width) for _ in range(intro_blocks)],
+            nn.Conv2d(width, 3, kernel_size=1))
+        
+        self.outro = nn.Sequential(nn.Conv2d(3, width, kernel_size=1),
+            nn.GELU(),
+            *[NAFBlock(width) for _ in range(outro_blocks)],
+            nn.Conv2d(width, 3, kernel_size=1))
+        
+        self.camera_rgb_adjust = torch.nn.Parameter(torch.tensor(rgb_multiplier).view(1, 3, 1, 1), requires_grad=True)
+        self.gamma = torch.nn.Parameter(torch.tensor([gamma]), requires_grad=True)
+
+    def to_proc(self, inp):
+        # To processed like
+        inp = inp ** self.gamma
+        inp = inp * self.camera_rgb_adjust
+        inp = self.intro(inp) + inp
+        return inp
+
+    def to_RAW(self, out):
+        # To RAW like
+        out = self.outro(out)
+        out = out /  self.camera_rgb_adjust
+        out = torch.clamp(out, 0.0, 1.0)
+
+        out = out ** (1./self.gamma)
+        return out
